12d40b7ec539fd0fa89d286068b443f409d06473
Jahfaith Irokanulo <52711069+Jahfaith@users.noreply.github.com>
2022-07-29 12:56:29 +0000

    created the css file.

---
+++ replit.nix
@@ -0,0 +1,16 @@
+/* setting the properties of the form name */
+h2 {
+  color: indigo;
+  background-color: gold;
+  padding: 20px 20px 0px 20px;
+  height: 50px;
+  width: 300px;
+  border-radius: 5px
+}
+
+/* setting the properties of the input button */
+input{
+  color: white;
+  background-color: blue;
+  border-radius: 25px
+}
